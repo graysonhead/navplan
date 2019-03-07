6fbb53c1ae09ea38c6448d8cd645025b19051018
Grayson Head <grayson@graysonhead.net>
2019-03-07 00:04:17 -0600

    Testing Env Variable Config

--- npserver/__init__.py
+++ npserver/__init__.py
@@ -1,14 +1,25 @@
 from flask import Flask, Blueprint
 from flask_sqlalchemy import SQLAlchemy
 import flask_restless
-import config
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 # Initialize flask app
 app = Flask(__name__,
             template_folder='./npclient/public',
             static_folder='./npclient/public',
             static_url_path='/static')
-app.config.from_object('config')
+
+db_test = os.environ.get("NP_DATABASE_SQLITE")
+db_uri = os.environ.get("NP_DATABASE_URI")
+if db_uri:
+    if db_test:
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app.db?check_same_thread=False')
+    else:
+        app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+else:
+    raise SyntaxError("You must specify a database URI")
 
 # Setup DB ORM
 print("Starting database")
