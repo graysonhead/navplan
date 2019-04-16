180916538a26a11b523cf9a46eb3d25d43ae4d15
Grayson Head <grayson@graysonhead.net>
2019-04-16 00:34:00 -0500

    Staring to Work on Map

--- npserver/npclient/src/components/FlightPlanMap.js
+++ npserver/npclient/src/components/FlightPlanMap.js
@@ -1,48 +1,52 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import ReactMapboxGl, { Layer, Feature } from "react-mapbox-gl";
-import { GeoJSONLayer } from "react-mapbox-gl";
+import ReactMap from 'react-mapbox-gl';
+import { Marker } from 'react-mapbox-gl';
+import Pin from "./map/pin";
 import 'mapbox-gl/dist/mapbox-gl.css';
 
-const Map = ReactMapboxGl({
-    accessToken: "pk.eyJ1IjoiZ3JheXNvbmhlYWQiLCJhIjoiY2p1Z2d0YjBoMHA2cDN5cGI4anYyYjN5dSJ9.WOkEIcr-x_6NEiua4U6A-w"
+const accessToken = "pk.eyJ1IjoiZ3JheXNvbmhlYWQiLCJhIjoiY2p1Z2d0YjBoMHA2cDN5cGI4anYyYjN5dSJ9.WOkEIcr-x_6NEiua4U6A-w";
+const style = "mapbox://styles/mapbox/streets-v9";
+
+
+const Map = ReactMap({
+    accessToken
 });
-console.log(Map);
-// Map.on('load', function() {
-//     Map.loadImage('https://i.imgur.com/MK4NUzI.png', function(error, image) {
-//         if (error) throw error;
-//         Map.addImage('marker', image);
-//     })
-// });
+
+const mapStyle = {
+    height: '50vh',
+    width: '100%'
+};
 
 class FlightPlanMap extends React.Component {
+
+
 
     getSteerpointLayers() {
         return this.props.flightplan.steerpoints.map(steerpoint => {
             console.log(steerpoint);
-            return (
-                <Layer
-                    type={"symbol"}
-                    id={steerpoint.id.toString()}
-                    layout={{ "icon-image": "marker-15" }}>
-                    <Feature coordinates={[steerpoint.longitude, steerpoint.latitude]}/>
-                </Layer>
-            )
+                return (
+                    <Marker
+                        key={steerpoint.id}
+                        coordinates={[steerpoint.longitude, steerpoint.latitude]}
+                        // latitude={steerpoint.latitude}
+                        // longitude={steerpoint.longitude}
+                        anchor={"bottom"}
+                    >
+                        <Pin size={20} />
+                    </Marker>
+                )
         })
     }
 
     render() {
         return (
-            <div>
-                <Map
-                    style="mapbox://styles/mapbox/streets-v9"
-                    containerStyle={{
-                        height: "50vh",
-                        width: "100%"
-                }}>
-                    {this.getSteerpointLayers()}
-                </Map>
-            </div>
+            <Map
+                style={style}
+                containerStyle={mapStyle}
+            >
+                {this.getSteerpointLayers()}
+            </Map>
         )
     }
 }
